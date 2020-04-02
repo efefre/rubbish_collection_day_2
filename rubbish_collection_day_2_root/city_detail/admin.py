054313dd08ece141cda2437f3f16455aaa461f74
Kasia <efefre@icloud.com>
2020-04-02 19:46:36 +0200

    Add CityAdmin and StreetAdmin

--- rubbish_collection_day_2_root/city_detail/admin.py
+++ rubbish_collection_day_2_root/city_detail/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from .models import City, Street
+
 
 # Register your models here.
+class CityAdmin(admin.ModelAdmin):
+    search_fields = ('name',)
+    ordering = ('name',)
+
+
+class StreetAdmin(admin.ModelAdmin):
+    search_fields = ('name',)
+    ordering = ('name',)
+
+
+admin.site.register(Street, StreetAdmin)
+admin.site.register(City, CityAdmin)
