fe49281704045fe28b161ffb83f084ac59b27f4c
Kasia <efefre@icloud.com>
2020-04-02 19:38:16 +0200

    Add class: City

--- rubbish_collection_day_2_root/city_detail/models.py
+++ rubbish_collection_day_2_root/city_detail/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
+
 # Create your models here.
+class City(models.Model):
+    name = models.CharField(max_length=40, verbose_name="Miejscowość")
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        ordering = ('name')
+        verbose_name_plural = "Miejscowości"
+        verbose_name = "Miejscowość"
