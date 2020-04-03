c75c4948e4d928d8120bff1ac8f8e988f1413fcd
Kasia <efefre@icloud.com>
2020-04-04 00:29:33 +0200

    Add model: Date

--- rubbish_collection_day_2_root/schedule/models.py
+++ rubbish_collection_day_2_root/schedule/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
+
 # Create your models here.
+class Date(models.Model):
+    date = models.DateField(verbose_name="Data")
+
+    def __str__(self):
+        return f"{self.date}"
+
+    class Meta:
+        verbose_name_plural = "Daty"
+        verbose_name = "Data"
