9f32db0a3608d195bbd4eaf81d58dac43fb43e85
Kasia <efefre@icloud.com>
2020-04-04 00:23:13 +0200

    Add schedule to INSTALLED_APPS

--- rubbish_collection_day_2_root/rubbish_collection_day_2/settings.py
+++ rubbish_collection_day_2_root/rubbish_collection_day_2/settings.py
@@ -38,7 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'city_detail'
+    'city_detail',
+    'schedule'
 ]
 
 MIDDLEWARE = [
