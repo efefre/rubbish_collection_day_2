2ed68e559ace2a3d06565ef37fdc23f99ece2b2a
Kasia <efefre@icloud.com>
2020-04-04 21:28:11 +0200

    Add __str__ for Street

--- rubbish_collection_day_2_root/city_detail/models.py
+++ rubbish_collection_day_2_root/city_detail/models.py
@@ -18,6 +18,9 @@ class City(models.Model):
 class Street(models.Model):
     name = models.CharField(max_length=100, verbose_name="Ulica")
 
+    def __str__(self):
+        return self.name
+
     class Meta:
         ordering = ("name",)
         verbose_name_plural = "Ulice"
