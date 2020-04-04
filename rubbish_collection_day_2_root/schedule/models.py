d23c19533e0d60397a73517ca4a4318595d1a0b1
Kasia <efefre@icloud.com>
2020-04-04 12:29:35 +0200

    Add class: RubbishType

--- rubbish_collection_day_2_root/schedule/models.py
+++ rubbish_collection_day_2_root/schedule/models.py
@@ -31,3 +31,41 @@ class RubbishType(models.Model):
         verbose_name = "Frakcja"
 
 
+class RubbishDistrict(models.Model):
+    CITY_TYPE_OPTION = [
+        ("gmina", "Gmina"),
+        ("miasto", "Miasto"),
+    ]
+
+    name = models.CharField(max_length=200, verbose_name="Nazwa")
+    city_type = models.CharField(
+        choices=CITY_TYPE_OPTION,
+        default="gmina",
+        verbose_name="Typ miejscowości",
+        max_length=10,
+    )
+    rubbish_type = models.ForeignKey(
+        RubbishType,
+        related_name="districts",
+        on_delete=models.SET_NULL,
+        null=True,
+        verbose_name="Frakcja",
+    )
+    date = models.ManyToManyField(
+        Date, related_name="districts", null=True, verbose_name="Daty"
+    )
+    address = models.ManyToManyField(
+        Address,
+        related_name="districts",
+        null=True,
+        verbose_name="Adres odbioru odpadów",
+        help_text="Adresy należące do tego regionu.",
+    )
+
+    def __str__(self):
+        return f"{self.name}"
+
+    class Meta:
+        verbose_name_plural = "Rejony odbioru odpadów"
+        verbose_name = "Rejon"
+        unique_together = ("name", "city_type", "rubbish_type")
