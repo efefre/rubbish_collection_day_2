415a74c327eeddaf4c7ea149902b892b379667e9
Kasia <efefre@icloud.com>
2020-04-04 12:29:04 +0200

    Add class: RubbishType

--- rubbish_collection_day_2_root/schedule/models.py
+++ rubbish_collection_day_2_root/schedule/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from city_detail.models import Address
 
 
 # Create your models here.
@@ -15,4 +16,18 @@ class Date(models.Model):
 
 class RubbishType(models.Model):
     name = models.CharField(max_length=200, verbose_name="Nazwa frakcji")
-    mark_color = models.CharField(max_length=7, verbose_name="Kolor zaznaczenia w kalendarzu")
+    mark_color = models.CharField(
+        max_length=7,
+        verbose_name="Kolor zaznaczenia w kalendarzu",
+        unique=True,
+        help_text="Podaj HEX zaczynająć od #. ",
+    )
+
+    def __str__(self):
+        return f"{self.name}"
+
+    class Meta:
+        verbose_name_plural = "Frakcje śmieci"
+        verbose_name = "Frakcja"
+
+
