cf4bd3cfe64a2f786f4a1bb247c89d809af4ff70
Kasia <efefre@icloud.com>
2020-04-05 18:27:26 +0200

    Add url for LoadStreetView

--- rubbish_collection_day_2_root/schedule/urls.py
+++ rubbish_collection_day_2_root/schedule/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
-from .views import HomeView
+from .views import HomeView, LoadStreetView
 
 app_name = "schedule"
 
-urlpatterns = [path("", HomeView.as_view(), name="home")]
+urlpatterns = [
+    path("", HomeView.as_view(), name="home"),
+    path('ajax/load-streets/', LoadStreetView.as_view(), name='ajax_load_streets'),
+    ]
