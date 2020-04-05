d01b2540c46995a7fe0e18be34cc06b374fb20f3
Kasia <efefre@icloud.com>
2020-04-05 17:28:00 +0200

    Add view: HomeView

--- rubbish_collection_day_2_root/schedule/views.py
+++ rubbish_collection_day_2_root/schedule/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.views.generic import FormView
+from .forms import ChooseAddress
+
 
 # Create your views here.
+class HomeView(FormView):
+    form_class = ChooseAddress
+    template_name = "schedule/home.html"
