f6409086564cab6e41331bb47e6ffb6f4e012006
Kasia <efefre@icloud.com>
2020-04-06 19:29:26 +0200

    Update ChooseAddress. Use city name as opption value

--- rubbish_collection_day_2_root/schedule/forms.py
+++ rubbish_collection_day_2_root/schedule/forms.py
@@ -3,10 +3,20 @@ from city_detail.models import Address
 
 
 class ChooseAddress(forms.ModelForm):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        choices = [(o.city, o.city) for o in Address.objects.all()]
+        self.fields["city"] = forms.ChoiceField(
+            choices=tuple([("", "---------")] + list(choices))
+        )
+        self.fields["city"].label = "Wybierz miejscowość"
+
     class Meta:
         model = Address
-        fields = ("city", "street")
+        fields = (
+            "city",
+            "street",
+        )
         labels = {
-            "city": "Wybierz miejscowość",
             "street": "Wybierz ulicę",
         }
