ef08e1419ca0de58536fbf8e5afe759b490aebe4
Kasia <efefre@icloud.com>
2020-04-07 18:17:44 +0200

    Add CalendarView

--- rubbish_collection_day_2_root/schedule/views.py
+++ rubbish_collection_day_2_root/schedule/views.py
@@ -1,9 +1,9 @@
 from django.views.generic import FormView, TemplateView
 from .forms import ChooseAddressForm
 from city_detail.models import Address
+from .utils import days_for_calendar
 
 
-# Create your views here.
 class HomeView(FormView):
     form_class = ChooseAddressForm
     template_name = "schedule/home.html"
@@ -14,8 +14,17 @@ class LoadStreetView(TemplateView):
     template_name = "schedule/street_dropdown_list_options.html"
 
     def get_context_data(self, **kwargs):
-        city_name = self.request.GET.get('city')
-        streets = Address.objects.filter(city__name=city_name).order_by('street')
+        city_name = self.request.GET.get("city")
+        streets = Address.objects.filter(city__name=city_name).order_by("street")
         context = super().get_context_data(**kwargs)
         context["streets"] = streets
+        return context
+
+
+class CalendarView(TemplateView):
+    template_name = "schedule/calendar.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["calendar"] = days_for_calendar(2020)
         return context
