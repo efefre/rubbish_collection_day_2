508c814ca06eda8cf857cca92e1897f0f2cf3855
Kasia <efefre@icloud.com>
2020-04-13 16:23:40 +0200

    Register filters: translate_month_to_pl and translate_day_to_pl

--- rubbish_collection_day_2_root/schedule/templatetags/calendar_extras.py
+++ rubbish_collection_day_2_root/schedule/templatetags/calendar_extras.py
@@ -6,3 +6,36 @@ register = template.Library()
 @register.filter
 def list_index(list, i):
     return list[int(i)]
+
+
+@register.filter
+def translate_month_to_pl(month):
+    months = {
+        "January": "Styczeń",
+        "February": "Luty",
+        "March": "Marzec",
+        "April": "Kwiecień",
+        "May": "Maj",
+        "June": "Czerwiec",
+        "July": "Lipiec",
+        "August": "Sierpień",
+        "September": "Wrzesień",
+        "October": "Październik",
+        "November": "Listopad",
+        "December": "Grudzień",
+    }
+    return months.get(month)
+
+
+@register.filter
+def translate_day_to_pl(day):
+    day_names = {
+        "Mon": "Pn",
+        "Tue": "Wt",
+        "Wed": "Śr",
+        "Thu": "Czw",
+        "Fri": "Pt",
+        "Sat": "Sob",
+        "Sun": "Ndz",
+    }
+    return day_names.get(day)
