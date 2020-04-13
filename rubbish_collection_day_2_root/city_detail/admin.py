5c6ad3292d119939bd0d7fd9d05cf3f711bba5a9
Kasia <efefre@icloud.com>
2020-04-13 15:32:51 +0200

    Rename and update: count_rubbish_district into: rubbish_district_status

--- rubbish_collection_day_2_root/city_detail/admin.py
+++ rubbish_collection_day_2_root/city_detail/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from django.utils.safestring import mark_safe
 from .models import City, Street, Address
+from schedule.models import RubbishType
 
 
 # Register your models here.
@@ -19,14 +20,13 @@ class AddressAdmin(admin.ModelAdmin):
         "street",
         "city",
         "get_rubbish_type_district",
-        "count_rubbish_district",
+        "rubbish_district_status",
     )
     search_fields = ("city__name", "street__name")
     ordering = ("city", "street")
     autocomplete_fields = ("city", "street")
     filter_horizontal = ("rubbish_district",)
     list_filter = (
-        "rubbish_district__rubbish_type",
         "rubbish_district__name",
         "rubbish_district__city_type",
         "city",
@@ -42,16 +42,46 @@ class AddressAdmin(admin.ModelAdmin):
         return mark_safe(
             " | ".join(
                 f"<b>{district.rubbish_type}</b> - {district.name} ({district.city_type.capitalize()})"
-                for district in obj.rubbish_district.all().order_by("rubbish_type")
+                for district in obj.rubbish_district.all()
+                .order_by("rubbish_type")
+                .select_related("rubbish_type")
             )
         )
 
     get_rubbish_type_district.short_description = "Przypisane rejony"
 
-    def count_rubbish_district(self, obj):
-        return obj.rubbish_district.count()
+    def rubbish_district_status(self, obj):
+        count_districts = obj.rubbish_district.count()
+        count_rubbish_types = RubbishType.objects.count()
+        if count_districts == count_rubbish_types:
+            city_type = obj.city.city_type
+            error_in_city_type = []
+            for district in obj.rubbish_district.all():
+                if district.city_type != city_type:
+                    error_in_city_type.append(district.rubbish_type)
 
-    count_rubbish_district.short_description = "Liczba rejonów"
+            if len(error_in_city_type) == 0:
+                return mark_safe(
+                    f"<span style='color:green'>Liczba zdefiniowanych rejonów: {count_districts}</style>"
+                )
+            else:
+                error_message = [
+                    ", ".join(rubbish_type.name for rubbish_type in error_in_city_type)
+                ]
+                return mark_safe(
+                    f"<span style='color:red'><b>Błąd</b>: {error_message[0]}</span>"
+                )
+
+        if count_districts < count_rubbish_types:
+            return mark_safe(
+                f"<span style='color:red'>Liczba zdefiniowanych rejonów: {count_districts} (za mało)</style>"
+            )
+        else:
+            return mark_safe(
+                f"<span style='color:red'>Liczba zdefiniowanych rejonów: {count_districts} (za dużo)</style>"
+            )
+
+    rubbish_district_status.short_description = "Status"
 
 
 admin.site.register(Street, StreetAdmin)
