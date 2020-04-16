74070da1676dad4b0988f86f152458d077cb0449
Kasia <efefre@icloud.com>
2020-04-16 22:20:47 +0200

    Add context for mark-rubbishgit status .css

--- rubbish_collection_day_2_root/schedule/views.py
+++ rubbish_collection_day_2_root/schedule/views.py
@@ -1,9 +1,10 @@
 from django.views.generic import FormView, TemplateView
 from .forms import ChooseAddressForm
 from city_detail.models import Address
-from schedule.models import RubbishDistrict
+from schedule.models import RubbishDistrict, RubbishType
 from .utils import days_for_calendar
 from collections import defaultdict
+from itertools import combinations
 
 
 class HomeView(FormView):
@@ -33,7 +34,11 @@ class CalendarView(TemplateView):
         address = Address.objects.select_related("city", "street").get(
             city__name=city_name, street__name=street_name
         )
-        rubbish_districts = RubbishDistrict.objects.select_related("rubbish_type").prefetch_related('date').filter(addresses=address)
+        rubbish_districts = (
+            RubbishDistrict.objects.select_related("rubbish_type")
+            .prefetch_related("date")
+            .filter(addresses=address)
+        )
         schedule_dates_for_address = defaultdict(list)
 
         for district in rubbish_districts:
@@ -44,5 +49,32 @@ class CalendarView(TemplateView):
         context["days_names_list"] = ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"]
         context["address"] = address
         context["schedule_dates_for_address"] = dict(schedule_dates_for_address)
+
+        return context
+
+
+class DynamicSvgCssView(TemplateView):
+    template_name = "schedule/css/mark-rubbish.css"
+    content_type = "text/css"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        rubbish_type = RubbishType.objects.all()
+
+        all_css_combination = []
+        rubbish_type_css = []
+        for rubbish in rubbish_type:
+            rubbish_type_css.append(rubbish.css_name)
+
+        i = 1
+        while i <= len(rubbish_type_css):
+            rub_combinations = combinations(rubbish_type_css, i)
+            for comb in rub_combinations:
+                all_css_combination.append(sorted(list(comb)))
+
+            i += 1
+        css_names = ["-".join(_) for _ in all_css_combination]
+
+        context["rubbish_type_css"] = css_names
 
         return context
