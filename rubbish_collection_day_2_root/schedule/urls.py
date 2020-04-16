4874743f411d0a7c2a4123442a443c42c63653d7
Kasia <efefre@icloud.com>
2020-04-16 22:22:42 +0200

    Add url for DynamicCssNameView

--- rubbish_collection_day_2_root/schedule/urls.py
+++ rubbish_collection_day_2_root/schedule/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import HomeView, LoadStreetView, CalendarView
+from .views import HomeView, LoadStreetView, CalendarView, DynamicCssNameView
 
 app_name = "schedule"
 
@@ -7,5 +7,6 @@ urlpatterns = [
     path("", HomeView.as_view(), name="home"),
     path('ajax/load-streets/', LoadStreetView.as_view(), 
          name='ajax_load_streets'),
-    path("calendar/", CalendarView.as_view(), name='calendar')
+    path("calendar/", CalendarView.as_view(), name='calendar'),
+    path('calendar/mark-rubbish.css', DynamicCssNameView.as_view(), name='mark-rubbish-css')
     ]
