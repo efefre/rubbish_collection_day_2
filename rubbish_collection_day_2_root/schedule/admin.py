db0cbdc18bb028088bb4fa733b96cdae12a05780
Kasia <efefre@icloud.com>
2020-04-25 22:36:17 +0200

    Update DateAdmin. Add form to generate dates

--- rubbish_collection_day_2_root/schedule/admin.py
+++ rubbish_collection_day_2_root/schedule/admin.py
@@ -1,12 +1,50 @@
-from django.contrib import admin
+from django.contrib import admin, messages
+from django.db import IntegrityError
+
+from django.urls import path
+from django.http import HttpResponseRedirect
 from .models import Date, RubbishType, RubbishDistrict, ScheduleConfiguration
 from solo.admin import SingletonModelAdmin
+from datetime import datetime, timedelta
 
 
 # Register your models here.
 class DateAdmin(admin.ModelAdmin):
     search_fields = ("date",)
     ordering = ("date",)
+    change_list_template = "admin/schedule/date/date_change_list.html"
+
+    def get_urls(self):
+        urls = super().get_urls()
+        custom_urls = [path("add-dates/", self.add_dates)]
+        return custom_urls + urls
+
+    def add_dates(self, request):
+        if request.method == "POST":
+            start_date = datetime.strptime(
+                request.POST.get("start-date"), "%Y-%m-%d"
+            ).date()
+            end_date = datetime.strptime(
+                request.POST.get("end-date"), "%Y-%m-%d"
+            ).date()
+
+            delta = end_date - start_date
+            add_new_dates = None
+            for i in range(delta.days + 1):
+                day = start_date + timedelta(days=i)
+
+                try:
+                    Date.objects.create(date=day)
+                except IntegrityError:
+                    self.message_user(
+                        request, f"Ta data już istnieje: {day}",
+                        level=messages.ERROR
+                    )
+                else:
+                    add_new_dates = True
+            if add_new_dates:
+                self.message_user(request, "Dodano nowe daty")
+        return HttpResponseRedirect("../")
 
 
 class RubbishDistrictAdmin(admin.ModelAdmin):
