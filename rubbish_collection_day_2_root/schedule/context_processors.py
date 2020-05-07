d99939cf20bd238c3246cbd741575c08d235a1e4
Kasia <efefre@icloud.com>
2020-05-07 20:55:26 +0200

    Update context_processors. Add config

--- rubbish_collection_day_2_root/schedule/context_processors.py
+++ rubbish_collection_day_2_root/schedule/context_processors.py
@@ -4,5 +4,5 @@ from schedule.models import ScheduleConfiguration
 def get_link_to_original_schedule(request):
     config = ScheduleConfiguration.get_solo()
     return {
-        "link_to_original_schedule": config.original_schedule
+        "config": config
     }
