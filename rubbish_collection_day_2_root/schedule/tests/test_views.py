bc9f730cfe581bc39fdbdc47d0f6ae2401802e32
Kasia <efefre@icloud.com>
2020-05-08 23:11:42 +0200

    Add: test_home_view

--- rubbish_collection_day_2_root/schedule/tests/test_views.py
+++ rubbish_collection_day_2_root/schedule/tests/test_views.py
@@ -4,6 +4,12 @@ from django.urls import reverse
 
 class TestViewsWithoutLogin:
     @pytest.mark.django_db
+    def test_home_view(self, client):
+        url = reverse("schedule:home")
+        response = client.get(url)
+        assert response.status_code == 200
+
+    @pytest.mark.django_db
     def test_ajax_load_streets(self, client):
         url = reverse("schedule:ajax_load_streets")
         response = client.get(url)
