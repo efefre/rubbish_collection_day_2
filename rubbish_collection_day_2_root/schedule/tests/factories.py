b46a46e2289a13d423c24ff4c590566115e4ab2e
Kasia <efefre@icloud.com>
2020-05-10 22:12:48 +0200

    Add RubbisTypeFactory

--- rubbish_collection_day_2_root/schedule/tests/factories.py
+++ rubbish_collection_day_2_root/schedule/tests/factories.py
@@ -1,21 +1,36 @@
 import datetime
 import factory
-from schedule.models import ScheduleConfiguration, Date
+from schedule.models import ScheduleConfiguration, Date, RubbishType
 
 
 class ScheduleConfigurationFactory(factory.django.DjangoModelFactory):
     FACTORY_FOR = ScheduleConfiguration
 
-    site_name = 'Test Page'
+    site_name = "Test Page"
     maintenance_mode = False
     year = 2020
-    original_schedule = 'www.original.localhost.pl'
+    original_schedule = "www.original.localhost.pl"
 
 
 class DateFactory(factory.django.DjangoModelFactory):
     FACTORY_FOR = Date
 
     date = factory.fuzzy.FuzzyDate(
-        datetime.date(2020, 1, 1),
-        datetime.date(2021, 3, 31),
+        datetime.date(2020, 1, 1), datetime.date(2021, 3, 31),
+    )
+
+
+class RubbishTypeFactory(factory.django.DjangoModelFactory):
+    FACTORY_FOR = RubbishType
+
+    name = factory.Iterator(
+        ["All Rubbish", "Bio Rubbish", "Recycled Rubbish",
+         "Ash", "Big Rubbish"],
+        cycle=False,
+    )
+    mark_color = factory.Iterator(
+        ["#F45B70", "#70CEAE", "#6095BD", "#9E9E9E", "#BC8F71"], cycle=False
+    )
+    css_name = factory.Iterator(
+        ["all", "bio", "rec", "ash", "big"], cycle=False
     )
