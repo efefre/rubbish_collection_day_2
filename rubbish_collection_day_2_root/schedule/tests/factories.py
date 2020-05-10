81f5face0850f6ba31a413f90e7ff4f82752446b
Kasia <efefre@icloud.com>
2020-05-10 21:58:47 +0200

    Add DateFactory

--- rubbish_collection_day_2_root/schedule/tests/factories.py
+++ rubbish_collection_day_2_root/schedule/tests/factories.py
@@ -1,5 +1,6 @@
+import datetime
 import factory
-from schedule.models import ScheduleConfiguration
+from schedule.models import ScheduleConfiguration, Date
 
 
 class ScheduleConfigurationFactory(factory.django.DjangoModelFactory):
@@ -9,3 +10,12 @@ class ScheduleConfigurationFactory(factory.django.DjangoModelFactory):
     maintenance_mode = False
     year = 2020
     original_schedule = 'www.original.localhost.pl'
+
+
+class DateFactory(factory.django.DjangoModelFactory):
+    FACTORY_FOR = Date
+
+    date = factory.fuzzy.FuzzyDate(
+        datetime.date(2020, 1, 1),
+        datetime.date(2021, 3, 31),
+    )
