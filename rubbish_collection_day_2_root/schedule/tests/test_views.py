f9dfa2ace08a63327b4a6d88b9fc8e9af7e126b9
Kasia <efefre@icloud.com>
2020-05-16 14:56:54 +0200

    Add TestMaintenanceModeOff: test_home_page_as_user, test_home_page_as_admin

--- rubbish_collection_day_2_root/schedule/tests/test_views.py
+++ rubbish_collection_day_2_root/schedule/tests/test_views.py
@@ -67,3 +67,30 @@ class TestMaintenanceModeOn:
             != "Przepraszamy. W tej chwili trwa przerwa techniczna.\nZapraszamy później."
         )
         assert city_label is not None
+
+
+@pytest.mark.django_db
+class TestMaintenanceModeOff:
+    def test_home_page_as_user(self, client):
+        maintenance_mode = factories.ScheduleConfigurationFactory(
+            maintenance_mode=False
+        )
+        response = (client.get(reverse("schedule:home"))).rendered_content
+
+        site = HTML(html=response)
+        alert_box = site.find("div.maintenance-mode", first=True)
+        city_label = site.find("#AddressForm > p:nth-child(1) > label", first=True)
+        assert alert_box is None
+        assert city_label is not None
+
+    def test_home_page_as_admin(self, admin_client):
+        maintenance_mode = factories.ScheduleConfigurationFactory(
+            maintenance_mode=False
+        )
+        response = (admin_client.get(reverse("schedule:home"))).rendered_content
+
+        site = HTML(html=response)
+        alert_box = site.find("div.maintenance-mode", first=True)
+        city_label = site.find("#AddressForm > p:nth-child(1) > label", first=True)
+        assert alert_box is None
+        assert city_label is not None
