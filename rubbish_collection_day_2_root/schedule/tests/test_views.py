1bb0ea15e0bff54e18cafc619e8600a1c5be91dd
Kasia <efefre@icloud.com>
2020-05-17 20:43:16 +0200

    Add TestCalendarView: add test_schedule_dates_for_address

--- rubbish_collection_day_2_root/schedule/tests/test_views.py
+++ rubbish_collection_day_2_root/schedule/tests/test_views.py
@@ -161,3 +161,58 @@ class TestMaintenanceModeOff:
         city_label = site.find("#AddressForm > p:nth-child(1) > label", first=True)
         assert alert_box is None
         assert city_label is not None
+
+
+@pytest.mark.django_db
+class TestCalendarView:
+    def test_schedule_dates_for_address(self, client):
+        date_all_1 = factories.DateFactory()
+        date_all_2 = factories.DateFactory()
+        district_all = factories.RubbishDistrictFactory.create(
+            date=(date_all_1, date_all_2)
+        )
+
+        date_bio_1 = factories.DateFactory()
+        date_bio_2 = factories.DateFactory()
+        district_bio = factories.RubbishDistrictFactory.create(
+            date=(date_bio_1, date_bio_2)
+        )
+
+        date_rec_1 = factories.DateFactory()
+        date_rec_2 = factories.DateFactory()
+        district_rec = factories.RubbishDistrictFactory.create(
+            date=(date_rec_1, date_rec_2)
+        )
+
+        date_ash_1 = factories.DateFactory()
+        date_ash_2 = factories.DateFactory()
+        district_ash = factories.RubbishDistrictFactory.create(
+            date=(date_ash_1, date_ash_2)
+        )
+
+        date_big_1 = factories.DateFactory()
+        date_big_2 = factories.DateFactory()
+        district_big = factories.RubbishDistrictFactory.create(
+            date=(date_big_1, date_big_2)
+        )
+
+        address = factories.AddressFactory.create(
+            rubbish_district=(
+                district_all,
+                district_ash,
+                district_big,
+                district_bio,
+                district_rec,
+            )
+        )
+
+        for district in address.rubbish_district.all():
+            assert district.date.count() == 2
+
+        url = reverse("schedule:calendar")
+        url = f"{url}?city={address.city}&street={address.street}"
+        response = (client.get(url)).content
+
+        site = HTML(html=response)
+        rubbish_marks = site.find("span.mark-rubbish")
+        assert len(rubbish_marks) == 10
