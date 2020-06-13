1f80a93c345a5554a6901a8104e01e9c0f506d9c
Kasia <efefre@icloud.com>
2020-06-13 19:05:40 +0200

    Add view: import_streets

--- rubbish_collection_day_2_root/import_data_for_schedule/views.py
+++ rubbish_collection_day_2_root/import_data_for_schedule/views.py
@@ -1,3 +1,15 @@
+from django.contrib import messages
 from django.shortcuts import render
+from .forms import UploadStreetsForm
+
 
 # Create your views here.
+def import_streets(request):
+    if request.method == 'POST':
+        form = UploadStreetsForm(request.POST, request.FILES)
+        if form.is_valid():
+            message =  messages.success(request, 'Plik został dodany')
+            render(request, 'import_data_for_schedule/upload_streets.html', {'form': form, 'message': message})
+    else:
+        form = UploadStreetsForm()
+    return render(request, 'import_data_for_schedule/upload_streets.html', {'form': form})
