9d3263d0a7b90eeb642e9a79cc2550de3c50323b
Kasia <efefre@icloud.com>
2020-06-13 19:37:57 +0200

    Add url for import_streets

--- rubbish_collection_day_2_root/import_data_for_schedule/urls.py
+++ rubbish_collection_day_2_root/import_data_for_schedule/urls.py
@@ -1,1 +1,9 @@
+from django.urls import path
+from . import views
+
 app_name = "import_data_for_schedule"
+
+
+urlpatterns = [
+    path("streets/", views.import_streets, name="import-streets"),
+]
