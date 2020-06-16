f484cf9d9746f7dd311d12241ad930d97fbff310
Kasia <efefre@icloud.com>
2020-06-16 22:16:27 +0200

    Add url for AddStreetToCityView

--- rubbish_collection_day_2_root/import_data_for_schedule/urls.py
+++ rubbish_collection_day_2_root/import_data_for_schedule/urls.py
@@ -6,4 +6,5 @@ app_name = "import_data_for_schedule"
 
 urlpatterns = [
     path("streets/", views.import_streets, name="import-streets"),
+    path("add-streets-to-city/", views.AddStreetToCityView.as_view(), name="add-streets-to-city"),
 ]
