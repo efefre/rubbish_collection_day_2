50fedba77f839beaaa4a0a88b42864a3eede3f5d
Kasia <efefre@icloud.com>
2020-06-16 22:15:31 +0200

    Add AddStreetToCityView

--- rubbish_collection_day_2_root/import_data_for_schedule/views.py
+++ rubbish_collection_day_2_root/import_data_for_schedule/views.py
@@ -1,8 +1,9 @@
 from django.contrib import messages
-from django.shortcuts import render
-from .forms import UploadStreetsForm
+from django.shortcuts import render, reverse
+from .forms import UploadStreetsForm, AddStreetsToCityForm
 from .utils import get_streets_names
-from city_detail.models import Street
+from city_detail.models import Street, City, Address
+from django.views.generic.edit import FormView
 
 
 # Create your views here.
@@ -61,3 +62,57 @@ def import_streets(request):
     return render(
         request, "import_data_for_schedule/upload_streets.html", {"form": form}
     )
+
+
+class AddStreetToCityView(FormView):
+    form_class = AddStreetsToCityForm
+    template_name = "import_data_for_schedule/add_streets.html"
+
+    def get_success_url(self):
+        return reverse("import_data_for_schedule:add-streets-to-city")
+
+    def form_valid(self, form):
+        streets = (form.cleaned_data["streets"]).replace(", ", ",").split(",")
+        city = form.cleaned_data["city"]
+        context = self.get_context_data()
+
+        new_streets = []
+        add_address = None
+        street_err = []
+
+        for street in streets:
+            city_id = City.objects.get(name=city)
+            try:
+                street_id = Street.objects.get(name=street.strip())
+            except Street.DoesNotExist:
+                Street.objects.create(name=street.strip())
+                new_streets.append(street)
+            else:
+                try:
+                    Address.objects.get(city=city_id, street=street_id)
+                except Address.DoesNotExist:
+                    Address.objects.create(city=city_id, street=street_id)
+                    add_address = True
+                else:
+                    street_err.append(street)
+
+        if new_streets:
+            message_warnings = messages.warning(
+                self.request, f'Dodano nowe ulice: {", ".join(new_streets)}'
+            )
+            context["message_warnings"] = message_warnings
+
+        if add_address:
+            message_success = messages.success(
+                self.request, f"Dodano nowe adresy w miejscowości {city}"
+            )
+            context["message_success"] = message_success
+
+        if street_err:
+            message_error = messages.error(
+                self.request,
+                f'Taki adres już istnieje. {city}: {", ".join(street_err)}',
+            )
+            context["message_error"] = message_error
+
+        return super().form_valid(form)
