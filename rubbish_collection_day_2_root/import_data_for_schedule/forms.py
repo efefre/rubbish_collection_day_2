2f6c4847f33343e4ebe187c533ec8cac85a6ef04
Kasia <efefre@icloud.com>
2020-06-16 22:14:58 +0200

    Add AddStreetToCityForm - create new address

--- rubbish_collection_day_2_root/import_data_for_schedule/forms.py
+++ rubbish_collection_day_2_root/import_data_for_schedule/forms.py
@@ -1,5 +1,11 @@
 from django import forms
+from city_detail.models import City
 
 
 class UploadStreetsForm(forms.Form):
     file = forms.FileField()
+
+
+class AddStreetsToCityForm(forms.Form):
+    city = forms.ModelChoiceField(queryset=City.objects.all(), empty_label=None)
+    streets = forms.CharField(widget=forms.Textarea)
