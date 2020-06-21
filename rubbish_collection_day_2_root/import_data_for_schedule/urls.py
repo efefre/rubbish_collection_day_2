c98aa15f11eed75317eb63bc154faf3f91b315ce
Kasia <efefre@icloud.com>
2020-06-21 14:32:49 +0200

    Add login_required for vies: import_streets, AddStreetToCityView

--- rubbish_collection_day_2_root/import_data_for_schedule/urls.py
+++ rubbish_collection_day_2_root/import_data_for_schedule/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
+from django.contrib.auth.decorators import login_required
 from . import views
 
 app_name = "import_data_for_schedule"
 
 
 urlpatterns = [
-    path("streets/", views.import_streets, name="import-streets"),
-    path("add-streets-to-city/", views.AddStreetToCityView.as_view(), name="add-streets-to-city"),
+    path("streets/", login_required(views.import_streets), name="import-streets"),
+    path("add-streets-to-city/", login_required(views.AddStreetToCityView.as_view()), name="add-streets-to-city"),
 ]
