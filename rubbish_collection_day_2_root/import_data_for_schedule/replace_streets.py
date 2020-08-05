26965f255b7f2d0f241d4fc6d16342d3a6f62ceb
Kasia <efefre@icloud.com>
2020-08-05 12:29:55 +0200

    Add dictionary with street maping. Used by AddStreetsToCityView

---
+++ rubbish_collection_day_2_root/import_data_for_schedule/replace_streets.py
@@ -0,0 +1,17 @@
+# Streets mapping.
+# Find street if import by AddStreetsToCityForm and replace name.
+# Necessary because sometimes get typo in same street name in different districts.
+
+wolomin_streets = {
+    "1 – go Maja": "1 Maja",
+    "1 –go Maja": "1 Maja",
+    "6 – go Września": "6 Września",
+    "6 -go Września": "6 Września",
+    "Legionów (od 1-go Maja do Stacji PKP)": "Legionów (od ul. Żelaznej do ul. 1 Maja)",
+    "Legionów (od 1-go Maja do Zielonej)": "Legionów (od ul. 1 Maja do lasu)",
+    "Legionów (od ul.1-go Maja do lasu)": "Legionów (od ul. 1 Maja do lasu)",
+    "Sikorskiego (od ul. 1-go Maja do lasu)": "Sikorskiego (od 1 Maja do Granicznej)",
+    "Sikorskiego (od ul. Wąskiej do ul. 1-go Maja)": "Sikorskiego (od 1 Maja do Wąskiej)",
+    "E.Plater": "E. Plater",
+    "Plater": "E. Plater",
+}
