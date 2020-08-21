363d7e79859d7cdb91ea4ef987931044f931a246
Kasia <efefre@icloud.com>
2020-08-21 15:23:06 +0200

    Update AddStreetToCityView. Check if field streets is empty

--- rubbish_collection_day_2_root/import_data_for_schedule/views.py
+++ rubbish_collection_day_2_root/import_data_for_schedule/views.py
@@ -116,8 +116,12 @@ class AddStreetToCityView(FormView):
         return reverse("import_data_for_schedule:add-streets-to-city")
 
     def form_valid(self, form):
-        streets = (form.cleaned_data["streets"]).replace(", ", ",").split(",")
+        if form.cleaned_data["streets"] !='':
+            streets = (form.cleaned_data["streets"]).replace(", ", ",").split(",")
+        else:
+            streets = []
         extra_street = form.cleaned_data["extra_street"]
+
         if extra_street:
             streets.append(extra_street)
         city = form.cleaned_data["city"]
