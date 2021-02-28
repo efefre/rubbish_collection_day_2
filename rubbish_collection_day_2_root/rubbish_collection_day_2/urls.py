7d18a75ce2da8e2f653abd372e11b79525cb1af4
Kasia <efefre@icloud.com>
2021-02-28 16:31:38 +0100

    Add redirect for urls with 'kalendarz'

--- rubbish_collection_day_2_root/rubbish_collection_day_2/urls.py
+++ rubbish_collection_day_2_root/rubbish_collection_day_2/urls.py
@@ -14,15 +14,18 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
 from django.conf import settings
 from django.conf.urls import handler404
+from django.views.generic.base import RedirectView
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", include("schedule.urls")),
     path("import-data/", include("import_data_for_schedule.urls")),
     path("info/", include("web_pages.urls")),
+    re_path('kalendarz/.*',
+         RedirectView.as_view(pattern_name='schedule:home')),
 ]
 
 if settings.DEBUG:
